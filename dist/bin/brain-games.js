378e3a2af8e292c76297c2255aeb72b981a855c0
mpokrovsky <duxast.98@gmail.com>
2018-10-01 20:32:57 +0300

    create function

--- dist/bin/brain-games.js
+++ dist/bin/brain-games.js
@@ -1,19 +1,11 @@
 #!/usr/bin/env node
 'use strict';
 
-var _readlineSync = require('readline-sync');
+var _ = require('..');
 
-var _readlineSync2 = _interopRequireDefault(_readlineSync);
+var _2 = _interopRequireDefault(_);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var sayHelloToUser = function sayHelloToUser() {
-    console.log("Welcome to the Brain Games!");
-    var userName = _readlineSync2.default.question('May I have your name? ');
-    console.log('Hello, ' + userName + '!');
-};
-
-//import sayHelloToUser from '..';
-
-sayHelloToUser();
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9iaW4vYnJhaW4tZ2FtZXMuanMiXSwibmFtZXMiOlsic2F5SGVsbG9Ub1VzZXIiLCJjb25zb2xlIiwibG9nIiwidXNlck5hbWUiLCJyZWFkbGluZVN5bmMiLCJxdWVzdGlvbiJdLCJtYXBwaW5ncyI6Ijs7QUFJQTs7Ozs7O0FBQ0EsSUFBTUEsaUJBQWlCLFNBQWpCQSxjQUFpQixHQUFNO0FBQ3pCQyxZQUFRQyxHQUFSLENBQVksNkJBQVo7QUFDQSxRQUFJQyxXQUFXQyx1QkFBYUMsUUFBYixDQUFzQix3QkFBdEIsQ0FBZjtBQUNBSixZQUFRQyxHQUFSLENBQVksWUFBWUMsUUFBWixHQUF1QixHQUFuQztBQUNILENBSkQ7O0FBSEE7O0FBUUFIIiwiZmlsZSI6ImJyYWluLWdhbWVzLmpzIiwic291cmNlc0NvbnRlbnQiOlsiXG5cbi8vaW1wb3J0IHNheUhlbGxvVG9Vc2VyIGZyb20gJy4uJztcblxuaW1wb3J0IHJlYWRsaW5lU3luYyBmcm9tICdyZWFkbGluZS1zeW5jJztcbmNvbnN0IHNheUhlbGxvVG9Vc2VyID0gKCkgPT4ge1xuICAgIGNvbnNvbGUubG9nKFwiV2VsY29tZSB0byB0aGUgQnJhaW4gR2FtZXMhXCIpXG4gICAgdmFyIHVzZXJOYW1lID0gcmVhZGxpbmVTeW5jLnF1ZXN0aW9uKCdNYXkgSSBoYXZlIHlvdXIgbmFtZT8gJyk7XG4gICAgY29uc29sZS5sb2coJ0hlbGxvLCAnICsgdXNlck5hbWUgKyAnIScpO1xufTtcbnNheUhlbGxvVG9Vc2VyKCk7XG4iXX0=
+(0, _2.default)();
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9iaW4vYnJhaW4tZ2FtZXMuanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7QUFFQTs7Ozs7O0FBRUEiLCJmaWxlIjoiYnJhaW4tZ2FtZXMuanMiLCJzb3VyY2VzQ29udGVudCI6WyJcblxuaW1wb3J0IHNheUhlbGxvVG9Vc2VyIGZyb20gJy4uJztcblxuc2F5SGVsbG9Ub1VzZXIoKTtcbiJdfQ==
