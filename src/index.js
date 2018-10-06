efcf418f9210d971a64f25a3633def2ca9b8cb82
mpokrovsky <duxast.98@gmail.com>
2018-10-06 14:36:56 +0300

    remove generateNum and getUserName

--- src/index.js
+++ src/index.js
@@ -1,18 +1,7 @@
 import readlineSync from 'readline-sync';
 import { car, cdr } from 'hexlet-pairs';
 
-export const getUserName = () => {
-  const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}!`);
-  return userName;
-};
-
-export const generateNum = (max) => {
-  const min = 1;
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-};
-
-const levels = (conditions, curLevel, attempts) => {
+const gameplay = (conditions, curLevel, attempts) => {
   const condition = conditions();
   const question = car(condition);
   const correctAnswer = cdr(condition);
@@ -29,22 +18,21 @@ const levels = (conditions, curLevel, attempts) => {
     console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}' `);
     return false;
   }
-  return levels(conditions, curLevel + 1, attempts);
+  return gameplay(conditions, curLevel + 1, attempts);
 };
 
 const attempts = 3;
 const curLevel = 0;
 
-export const game = (rule, conditions) => {
+export default (rule, conditions) => {
   console.log('Welcome to the Brain Games!');
   console.log(rule);
-  const userName = getUserName();
-  const isWin = levels(conditions, curLevel + 1, attempts);
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+  const isWin = gameplay(conditions, curLevel + 1, attempts);
   if (isWin) {
     console.log(`Congratulations, ${userName}!`);
   } else {
     console.log(`Let's try again, ${userName}!`);
   }
 };
-
-// export default game;
